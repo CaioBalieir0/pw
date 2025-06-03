6a057eebffe49b6190dd25d141c9cc46bad6a05e
Caio Balieiro <caiobalieiro676@gmail.com>
2025-06-03 15:05:51 -0300

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+});
